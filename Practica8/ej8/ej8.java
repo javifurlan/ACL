882c5fbd6fa960196fee1fa6155ee48748fab8f5
gustavo <gmartinezgranella@gmail.com>
2014-10-16 15:24:59 -0300

    [#80849374]implementedExcersice8

---
+++ Practica8/ej8/ej8.java
@@ -0,0 +1,23 @@
+package ej8;
+
+
+
+public class ej8 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Par k;
+		int i=0;
+		int j=2*i;
+		
+		while(true){
+			k=new Par(i,j);
+			System.out.println("objeto creado n°"+k.getId());
+		}
+		
+		
+		
+		
+	}
+
+}
